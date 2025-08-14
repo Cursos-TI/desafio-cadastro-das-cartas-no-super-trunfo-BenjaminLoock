dbcb26cd2a956e3a86e821eea59c84622f398bf8
BenjaminLoock <andresantosprogti@gmail.com>
2025-08-14 00:07:18 +0000

    int main() {     int População;     float Área;     float PIB;     int NúmeroDePontosTurísticos;

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,13 @@
 //Teste larissa
 
 int main() {
+    int População;
+    float Área;
+    float PIB;
+    int NúmeroDePontosTurísticos;
+
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
